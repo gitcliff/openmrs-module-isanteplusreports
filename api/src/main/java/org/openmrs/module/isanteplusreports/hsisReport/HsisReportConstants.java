954e1b9bbf61cbf16ca0d3f5d8335bd2d005dcff
mozzy11 <mozzymutesa@gmail.com>
2021-08-09 21:50:45 +0300

    hsis report innitial commit

---
+++ api/src/main/java/org/openmrs/module/isanteplusreports/hsisReport/HsisReportConstants.java
@@ -0,0 +1,9 @@
+package org.openmrs.module.isanteplusreports.hsisReport;
+
+public class HsisReportConstants {
+
+    public static final String HSIS_REPORT_UUID = "15861dcc-f817-11eb-9a03-0242ac130003";
+
+    public final static String DATA_SET_NAME_VISTS_DISTRIBUTION = "distribution 0f vists";
+    
+}
